6ceb77e4382b775db11d8c90a77c2ba0fcbd965a
Shawn Sullivan <sullivansm3@gmail.com>
2020-11-15 13:36:55 -0700

    Render text detected from Google's Doc AI

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,42 @@ const url: string = 'http://localhost:8080';
 
 function App() {
   const [file, setFile] = useState<any>(null);
+  const [pdfText, setPdfText] = useState<string[]>([]);
 
   const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    console.log(e.target.files![0])
-
     setFile(e.target.files![0]);
   }
 
   const onFileUpload = () => {
     const dataFile = new FormData();
     dataFile.append('file', file)
-    console.log(dataFile)
 
     axios.post(`${url}/invoice-scanner`, dataFile)
       .then( ({ data }) => {
         console.log(data)
+        setPdfText(data);
       })
       .catch( err => console.log(err))
   }
 
   return (
     <div className="App">
-      Invoice UI
+      <h1>Invoice UI</h1>
       <div> 
         <input type="file" onChange={onFileChange} /> 
         <button onClick={onFileUpload}> 
             Upload 
         </button> 
-      </div> 
+      </div>
+      {pdfText.length > 0 && 
+        <div style={{textAlign: "left", marginLeft: "10px"}}>
+          <h3>Text in Document:</h3>
+          {pdfText.map((textGrouping, index) => (
+            <p key={index}>{textGrouping}</p>
+          ))}
+        </div>
+      } 
     </div>
   );
 }
